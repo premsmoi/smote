7f348bce21b1ab19cff1370861d23849159ed150
Passakorn Hengprasert <passakorn.heng@gmail.com>
2023-07-18 01:20:08 +0700

    Remove setBoard on BoardItem

--- src/common/components/boardItem.tsx
+++ src/common/components/boardItem.tsx
@@ -1,24 +1,21 @@
 import { Paper, Typography } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { boardAtom } from '../../atoms/board';
 
 interface Props {
-    board: Board;
+  board: Board;
 }
 
-const BoardItem: React.FC<Props> = props => {
-    const { board } = props;
-    const [, setBoard] = useRecoilState(boardAtom);
+const BoardItem: React.FC<Props> = (props) => {
+  const { board } = props;
 
-    return (
-        <Paper className='boardItem' elevation={3}>
-            <Link onClick={() => setBoard(board)} href={{ pathname: `/board/${board.boardId}` }}>
-                <Typography>{board.boardName}</Typography>
-            </Link>
-        </Paper>
-    );
+  return (
+    <Paper className="boardItem" elevation={3}>
+      <Link href={{ pathname: `/board/${board.boardId}` }}>
+        <Typography>{board.boardName}</Typography>
+      </Link>
+    </Paper>
+  );
 };
 
 export default BoardItem;
